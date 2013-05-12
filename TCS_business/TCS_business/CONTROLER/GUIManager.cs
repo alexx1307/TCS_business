159a949bfb7a2ee39acbc0c2af7f6b2517897486
alexx <alexx1307@gmail.com>
2013-05-12 13:25:47 +0200

    Changes with todo tasks

--- TCS_business/TCS_business/CONTROLER/GUIManager.cs
+++ TCS_business/TCS_business/CONTROLER/GUIManager.cs
@@ -11,23 +11,23 @@ namespace TCS_business.CONTROLER
 
     class GuiManager          //maybe should this class  use singleton pattern? 
     {
-        //todo: Anita z wyjatkiem jednej metody na razie
 
         public static GuiManager getGUIManager()
         {
             return new GuiManager();
         }
-        public int InitializeMainWindow() { throw new NotImplementedException(); } //returns 0 if the window is correctly initialized  //todo: pawel
+        public int InitializeMainWindow() { throw new NotImplementedException(); } //todo: pawel //returns 0 if the window is correctly initialized  //todo: pawel
         public void ShowMainWindow() { throw new NotImplementedException(); } // todo: pawel
-        public GameConfig ShowGameConfigDialog() { throw new NotImplementedException(); } //if config data is filled incorrectly this method should return null!
-        public Player ShowAddPlayerDialog() { throw new NotImplementedException(); } //napisalas ze juz zrobilas wiec wystarczy tylko podpiac pod te metode 
+        public GameConfig ShowGameConfigDialog() { throw new NotImplementedException(); } //todo: Anita //if config data is filled incorrectly this method should return null!
+        public Player ShowAddPlayerDialog() { throw new NotImplementedException(); } // todo: Anita //napisalas ze juz zrobilas wiec wystarczy tylko podpiac pod te metode 
+        //todo: pawel proste funkcje zmieniajace stan okienka np umozliwienie lub zablokowanie wywolania jakichs okienek itd
         public void ShowErrorMessage(String msg) //mozesz zmienic te funkcje zeby poprawnie korzystala z watku od GUI - zrobilem ja na szybko gdy potrzebowalem
         {
             MessageBox.Show(msg);
             TCSBusinessApplication.Exit();
         }
 
-        internal void EnableAddPlayerButton()
+        internal void EnableAddPlayerButton()  
         {
             throw new NotImplementedException();
         }
@@ -43,6 +43,13 @@ namespace TCS_business.CONTROLER
         }
 
         internal void DisableAddPlayerButton()
+        {
+            throw new NotImplementedException();
+        }
+
+        //this method invokes GUI methods to update and repaint main game view 
+        //(when for example turn was changed or someone buy something and it's cash changed)
+        void UpdateGameView()   
         {
             throw new NotImplementedException();
         }
