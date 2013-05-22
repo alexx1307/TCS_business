f3251ee72483fb9b782aaf8906d7c0bc1558de2c
alexx <alexx1307@gmail.com>
2013-05-22 18:00:34 +0200

    Dodanie kilku klas

---
+++ TCS_business/TCS_business/VIEW/BoardPanel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TCS_business.MODEL;
+
+namespace TCS_business.VIEW
+{
+    public partial class BoardPanel : UserControl
+    {
+        public BoardPanel()
+        {
+            InitializeComponent();
+        }
+        public void Initialize(Board board)
+        {
+            if(Board.NOFIELDS%4 !=0){
+                throw new Exception("Cannot create board - incorrect fields number");
+            }
+
+        }
+    }
+}
---
+++ TCS_business/TCS_business/VIEW/FieldPanel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TCS_business.VIEW
+{
+    public partial class FieldPanel : UserControl
+    {
+        public FieldPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
